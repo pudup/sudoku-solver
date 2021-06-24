2007c1eb53d29d489bd078d8027ab48735a5f049
pudup <jasontauro@gmail.com>
2021-06-24 16:41:17 +0530

    Add visual for solver and clean leftover lines

--- solver.py
+++ solver.py
@@ -1,4 +1,5 @@
 import os
+from time import sleep
 
 example_grid = [[0, 0, 0, 8, 0, 1, 0, 0, 0],
                 [0, 0, 0, 0, 0, 0, 0, 4, 3],
@@ -22,6 +23,8 @@ def pretty_matrix(array):
             print(line)
             line = ""
         for count, value in enumerate(i):
+            if value == 0:
+                value = "_"
             if count == 2 or count == 5:
                 line += str(value) + " "
                 line += "|" + " "
@@ -45,25 +48,29 @@ def helperfunc(number, position_x, position_y, grid):  # Return true if number w
     for i in range(0, 3):  # Checks if number exists in the box
         for j in range(0, 3):
             if grid[((x // 3) * 3)+i][((y // 3) * 3)+j] == number:
-                # print("False")
                 return False
     return True
 
 
-def sudoku_solver(grid):
+def sudoku_solver(grid, visual=0.0):
     for i in range(0, 9):  # Loop through X positions
         for j in range(0, 9):  # Loop through Y positions
             if grid[i][j] == 0:  # Check if position is empty
                 for k in range(1, 10):  # Try all numbers in empty position using helper function
                     if helperfunc(number=k, position_x=i, position_y=j, grid=grid):
                         grid[i][j] = k  # If true then set position to working number
-                        can_continue = sudoku_solver(grid)  # Recurse
+                        if visual:
+                            os.system('cls' if os.name == 'nt' else 'clear')
+                            pretty_matrix(grid)
+                            sleep(visual)
+                        can_continue = sudoku_solver(grid, visual)  # Recurse
                         if can_continue:
                             return True
 
                         grid[i][j] = 0  # If dead end then reset
                 return False  # Return when no 0s in grid
 
+    print("\n")
     pretty_matrix(grid)  # Print a working solution
     more = input("\nY/N for more solutions: ") # Currently seems to go on forever if given grid only has one solution when Y is chosen
     if more.lower() == "y":
@@ -72,4 +79,4 @@ def sudoku_solver(grid):
         return True
 
 
-sudoku_solver(example_grid)
+sudoku_solver(example_grid, 0.01)
