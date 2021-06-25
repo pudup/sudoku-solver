2fa0599a0f9952dbd3cb3bbd970afc71753591bc
pudup <jasontauro@gmail.com>
2021-06-25 17:39:13 +0530

    Add name, icon and grid

--- pygameUI.py
+++ pygameUI.py
@@ -6,6 +6,17 @@ pygame.init()
 # Create window
 screen = pygame.display.set_mode((800,800))
 
+# Window Title
+pygame.display.set_caption("Didi Game Game")
+
+# Window Icon
+icon = pygame.image.load("icon.png")
+pygame.display.set_icon(icon)
+
+# Grid image
+grid_image = pygame.image.load('backgr6.png')
+
+
 # Window loop
 is_running = True
 while is_running:
@@ -14,3 +25,8 @@ while is_running:
             running = False
             pygame.quit()
             sys.exit()
+
+    screen.fill((200, 200, 200))
+
+    screen.blit(grid_image, (100, 100))
+    pygame.display.update()
