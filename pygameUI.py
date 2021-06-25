81d057033edf0c80b979308279bc6baddccc1246
pudup <jasontauro@gmail.com>
2021-06-25 17:15:15 +0530

    Make window

--- pygameUI.py
+++ pygameUI.py
@@ -1,2 +1,16 @@
-import pygame
+import pygame, sys
 
+# Initialize Pygame
+pygame.init()
+
+# Create window
+screen = pygame.display.set_mode((800,800))
+
+# Window loop
+is_running = True
+while is_running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+            pygame.quit()
+            sys.exit()
