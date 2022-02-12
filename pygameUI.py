c7e3bf493e4947246e34d5da29df621dbbe79462
pudup <jasontauro@gmail.com>
2022-02-12 13:29:47 +0530

    Use functions instead

--- pygameUI.py
+++ pygameUI.py
@@ -13,17 +13,16 @@ NUMBERS_COL = (230, 9, 101)
 response = requests.get("https://sugoku.herokuapp.com/board?difficulty=easy")
 board = response.json()["board"]
 
-# Main loop
-def main():
-    pygame.init()
+## Functions
+# Input numbers
+def input_num(window, position):
+    font = pygame.font.SysFont("Verdana", 64)
+    num = font.render(str(1), True, NUMBERS_COL)
+    window.blit(num, (position[0] * 80 + 24, position[1] * 80 + 12))
+    pygame.display.update()
 
-    #Window Settings
-    window = pygame.display.set_mode((885,900))
-    pygame.display.set_caption("Sudoku Solver")
-    window.fill(BACKGROUND_COL)
-    font = pygame.font.SysFont("Verdana", 60)
-
-    #Grid Settings
+#Grid Settings
+def draw_grid(window):
     for i in range(10): # Can be written in one loop if using a single colour
         if i % 3 != 0: # Remove unnecessary lines
             pygame.draw.line(window, THIN_LINES_COL, (80 + (80 * i), 80), (80 + (80 * i), 800), 2)
@@ -32,15 +31,30 @@ def main():
         if i % 3 == 0:
             pygame.draw.line(window, THICC_LINES_COL, (80 + (80 * i), 80), (80 + (80 * i), 801), 4)# 801 to smoothen corner
             pygame.draw.line(window, THICC_LINES_COL, (80, 80 + (80 * i)), (801, 80 + (80 * i)), 4)
-    pygame.display.update() # Redundant?
+    pygame.display.update()
 
-    #Grid populate
+#Grid populate
+def grid_init(window, font):
     for y in range(0, len(board[0])):
         for x in range(0, len(board[0])):
             if board[y][x] != 0:
                 num = font.render(str(board[y][x]), True, NUMBERS_COL)
-                window.blit(num, (((x + 1) * 80) + 24, ((y + 1) * 80) + 16))
+                window.blit(num, (((x + 1) * 80) + 24, ((y + 1) * 80) + 12))
     pygame.display.update()
+
+# Main loop
+def main():
+    pygame.init()
+
+    #Window Settings
+    window = pygame.display.set_mode((885,900))
+    pygame.display.set_caption("Sudoku Solver")
+    window.fill(BACKGROUND_COL)
+    font = pygame.font.SysFont("Verdana", 64)
+
+    draw_grid(window=window)
+    grid_init(window=window, font=font)
+
 
     # Board loop
     while True:
@@ -55,8 +69,13 @@ def main():
                         for x in range(0, len(board[0])):
                             if board[y][x] != 0:
                                 num = font.render(str(board[y][x]), True, NUMBERS_COL)
-                                window.blit(num, (((x + 1) * 80) + 24, ((y + 1) * 80) + 16))
+                                window.blit(num, (((x + 1) * 80) + 24, ((y + 1) * 80) + 12))
                     pygame.display.update()
+            if event.type == pygame.MOUSEBUTTONUP and event.button == 1:
+                position = pygame.mouse.get_pos()
+                input_num(window, (position[0]//80, position[1]//80))
+
+
 
 
 
