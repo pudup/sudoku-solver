390a6c48035250d46551c5b6a04bef33a1ec2ba5
pudup <jasontauro@gmail.com>
2022-02-12 11:26:13 +0530

    Draw and colour grid

--- pygameUI.py
+++ pygameUI.py
@@ -5,12 +5,19 @@ def main():
     pygame.init()
 
     #Window Settings
-    window = pygame.display.set_mode((600,600))
+    window = pygame.display.set_mode((885,900))
     pygame.display.set_caption("Sudoku Solver")
-    window.fill((255,255,255))
+    window.fill((251, 229, 229))
 
     #Grid Settings
-
+    for i in range(10): # Can be written in one loop if using a single colour
+        pygame.draw.line(window, (255,161,201), (80 + (80 * i), 80), (80 + (80 * i), 800), 2)
+        pygame.draw.line(window, (255,161,201), (80, 80 + (80 * i)), (800, 80 + (80 * i)), 2)
+    for i in range(10): # Extra loop written to avoid thin lines being drawn over thick lines (visually less attractive)
+        if i % 3 == 0:
+            pygame.draw.line(window, (249, 72, 146), (80 + (80 * i), 80), (80 + (80 * i), 801), 4)
+            pygame.draw.line(window, (249, 72, 146), (80, 80 + (80 * i)), (801, 80 + (80 * i)), 4)
+    pygame.display.update()
 
     while True:
         for event in pygame.event.get():
