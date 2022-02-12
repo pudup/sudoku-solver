3b7637846f15008f1c57055126d306e4efdf4b23
pudup <jasontauro@gmail.com>
2022-02-13 02:23:17 +0530

    Add highlighted squares

--- pygameUI.py
+++ pygameUI.py
@@ -23,16 +23,47 @@ user_board = deepcopy(board)
 # Input numbers
 def add_a_num(window, position, event):
     font = pygame.font.SysFont("Verdana", 64)
+    if user_board[position[1]- 1][position[0] - 1] == event.key - 48:
+        user_board[position[1]- 1][position[0] - 1] = 0
     if helperfunc(event.key - 48, position[1] - 1, position[0] - 1, user_board):
         num = font.render(str(event.key - 48), True, (36, 161, 156))
     else:
         num = font.render(str(event.key - 48), True, (251, 116, 62))
+    clear_square(window=window, position=(Y, X))
     window.blit(num, (position[0] * 80 + 24, position[1] * 80 + 12))
     pygame.display.update()
 
 def clear_square(window, position):
     x, y = position[1], position[0]
     pygame.draw.rect(window, BACKGROUND_COL, (y * 80 + 10, x * 80 + 5, 60, 70))
+
+def clean_board(window, font):
+    window.fill(BACKGROUND_COL)
+    for i in range(10):  # Can be written in one loop if using a single colour
+        if i % 3 != 0:  # Remove unnecessary lines
+            pygame.draw.line(window, THIN_LINES_COL, (80 + (80 * i), 80), (80 + (80 * i), 800), 2)
+            pygame.draw.line(window, THIN_LINES_COL, (80, 80 + (80 * i)), (800, 80 + (80 * i)), 2)
+    for i in range(
+            10):  # Extra loop written to avoid thin lines being drawn over thick lines (visually less attractive)
+        if i % 3 == 0:
+            pygame.draw.line(window, THICC_LINES_COL, (80 + (80 * i), 80), (80 + (80 * i), 801),
+                             4)  # 801 to smoothen corner
+            pygame.draw.line(window, THICC_LINES_COL, (80, 80 + (80 * i)), (801, 80 + (80 * i)), 4)
+    for y in range(0, len(user_board[0])):
+        for x in range(0, len(user_board[0])):
+            if user_board[y][x] != board[y][x]:
+                old_val = user_board[y][x]
+                user_board[y][x] = 0
+                if helperfunc(old_val, y, x, user_board):
+                    num = font.render(str(old_val), True, (36, 161, 156))
+                else:
+                    num = font.render(str(old_val), True, (251, 116, 62))
+                user_board[y][x] = old_val
+                clear_square(window=window, position=(x + 1, y + 1))
+                window.blit(num, ((x + 1) * 80 + 24, (y + 1) * 80 + 12))
+            elif user_board[y][x] != 0:
+                num = font.render(str(user_board[y][x]), True, NUMBERS_COL)
+                window.blit(num, (((x + 1) * 80) + 24, ((y + 1) * 80) + 12))
     pygame.display.update()
 
 
@@ -47,6 +78,13 @@ def draw_grid(window):
             pygame.draw.line(window, THICC_LINES_COL, (80 + (80 * i), 80), (80 + (80 * i), 801), 4)# 801 to smoothen corner
             pygame.draw.line(window, THICC_LINES_COL, (80, 80 + (80 * i)), (801, 80 + (80 * i)), 4)
     pygame.display.update()
+
+def highlight_square(window, position):
+    x, y = position[1], position[0]
+    for i in range(2):
+        pygame.draw.line(window, (193, 222, 174), ((i * 80) + x * 80, y * 80), ((i * 80) + x * 80, y * 80 + 80), 8)
+        pygame.draw.line(window, (193, 222, 174), (x * 80, (i * 80) + y * 80), (x * 80 + 80, (i * 80) + y * 80), 8)
+        pygame.display.update()
 
 #Grid populate
 def grid_init(window, font):
@@ -85,7 +123,7 @@ def main():
                 pygame.quit()
                 return
             if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_SPACE:
+                if event.key == pygame.K_SPACE: # AUTO SOLVER
                     clr_scr(window=window)
                     draw_grid(window=window)
                     sudoku_solver(board)
@@ -100,8 +138,11 @@ def main():
                 x, y = position[1] // 80, position[0] // 80
                 if ((y * 80) < 80) or ((x * 80) < 80) or ((y * 80) >= 800) or ((x * 80) >= 800):
                     X, Y = 0, 0
+                    clean_board(window, font)
                 else:
                     X, Y = position[1]//80, position[0]//80
+                    clean_board(window, font)
+                    highlight_square(window, (X, Y))
             if event.type == pygame.KEYDOWN:
                 if (X, Y) == (0, 0):
                     pass
@@ -110,8 +151,8 @@ def main():
                 elif event.key == 48:
                     clear_square(window=window, position=(Y, X))
                     user_board[X-1][Y-1] = 0
+                    pygame.display.update()
                 elif 0 < (event.key - 48) < 10: # ASCII Value ';..;'
-                    clear_square(window=window, position=(Y, X))
                     add_a_num(window, (y, x), event)
                     user_board[X-1][Y-1] = event.key - 48
 
