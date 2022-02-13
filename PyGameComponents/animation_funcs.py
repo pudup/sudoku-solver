18c1744ce6290ee9371faf072c688c729d3e0c85
pudup <jasontauro@gmail.com>
2022-02-13 15:27:03 +0530

    Smoothen highlighted square edges

--- PyGameComponents/animation_funcs.py
+++ PyGameComponents/animation_funcs.py
@@ -18,7 +18,7 @@ def clear_square(window, position):
     x, y = position[1], position[0]
     pygame.draw.rect(window, BACKGROUND_COL, (y * 80 + 10, x * 80 + 5, 60, 70))
 
-def update_board(window, font, user_board, board):
+def update_board(window, font, user_board, board, solver=False, always_unsolved=False):
     window.fill(BACKGROUND_COL)
     for i in range(10):  # Can be written in one loop if using a single colour
         if i % 3 != 0:  # Remove unnecessary lines
@@ -32,19 +32,28 @@ def update_board(window, font, user_board, board):
             pygame.draw.line(window, THICC_LINES_COL, (80, 80 + (80 * i)), (801, 80 + (80 * i)), 4)
     for y in range(0, len(user_board[0])):
         for x in range(0, len(user_board[0])):
-            if user_board[y][x] != board[y][x]:
-                old_val = user_board[y][x]
-                user_board[y][x] = 0
-                if helperfunc(old_val, y, x, user_board):
-                    num = font.render(str(old_val), True, (36, 161, 156))
-                else:
-                    num = font.render(str(old_val), True, (251, 116, 62))
-                user_board[y][x] = old_val
-                clear_square(window=window, position=(x + 1, y + 1))
-                window.blit(num, ((x + 1) * 80 + 24, (y + 1) * 80 + 12))
-            elif user_board[y][x] != 0:
-                num = font.render(str(user_board[y][x]), True, NUMBERS_COL)
-                window.blit(num, (((x + 1) * 80) + 24, ((y + 1) * 80) + 12))
+            if solver == True:
+                if user_board[y][x] != always_unsolved[y][x]:
+                    num = font.render(str(user_board[y][x]), True, (36, 161, 156))
+                    clear_square(window=window, position=(x + 1, y + 1))
+                    window.blit(num, ((x + 1) * 80 + 24, (y + 1) * 80 + 12))
+                elif user_board[y][x] != 0:
+                    num = font.render(str(user_board[y][x]), True, NUMBERS_COL)
+                    window.blit(num, (((x + 1) * 80) + 24, ((y + 1) * 80) + 12))
+            else:
+                if user_board[y][x] != board[y][x]:
+                    old_val = user_board[y][x]
+                    user_board[y][x] = 0
+                    if helperfunc(old_val, y, x, user_board):
+                        num = font.render(str(old_val), True, (36, 161, 156))
+                    else:
+                        num = font.render(str(old_val), True, (251, 116, 62))
+                    user_board[y][x] = old_val
+                    clear_square(window=window, position=(x + 1, y + 1))
+                    window.blit(num, ((x + 1) * 80 + 24, (y + 1) * 80 + 12))
+                elif user_board[y][x] != 0:
+                    num = font.render(str(user_board[y][x]), True, NUMBERS_COL)
+                    window.blit(num, (((x + 1) * 80) + 24, ((y + 1) * 80) + 12))
     insturctions(window)
     pygame.display.update()
 
@@ -52,7 +61,7 @@ def highlight_square(window, position):
     x, y = position[1], position[0]
     for i in range(2):
         pygame.draw.line(window, (193, 222, 174), ((i * 80) + x * 80, y * 80), ((i * 80) + x * 80, y * 80 + 80), 8)
-        pygame.draw.line(window, (193, 222, 174), (x * 80, (i * 80) + y * 80), (x * 80 + 80, (i * 80) + y * 80), 8)
+        pygame.draw.line(window, (193, 222, 174), (x * 80 - 3, (i * 80) + y * 80), (x * 80 + 84, (i * 80) + y * 80), 8)
         pygame.display.update()
 
 def insturctions(window):
