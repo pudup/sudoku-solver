40d61ee0b8ae8d790d4af84f04685542983bc3f0
pudup <jasontauro@gmail.com>
2022-02-14 15:38:08 +0530

    Fix path

--- TkinterVersion/solvergui.py
+++ TkinterVersion/solvergui.py
@@ -1,13 +1,14 @@
 import tkinter
 import os
 from solverUIHelp import sudoku_solver
+import sys
 
 
 def resource_path(relative_path):
     try:
         base_path = sys._MEIPASS
     except Exception:
-        base_path = os.path.abspath("..")
+        base_path = os.path.abspath(".")
 
     return os.path.join(base_path, relative_path)
 
